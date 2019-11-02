68aeab7db5169c5e5234c14bedb3ac90d7c882bd
Chris Hamper <chris.hamper@acquia.com>
2019-11-02 16:59:10 -0400

    Tweak syntax highlighting, fix command prompt styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,8 @@
 'use strict';
 
+require("prismjs/plugins/line-numbers/prism-line-numbers.css");
+require("prismjs/plugins/command-line/prism-command-line.css")
 require('./src/assets/scss/init.scss');
 require('./static/css/prismjs/theme.min.css');
+
+require("prismjs/themes/prism-tomorrow.css");
