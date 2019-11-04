8d5e4b6948ab74c3f324840c5ecea86455b8f36d
Chris Hamper <chris.hamper@acquia.com>
2019-11-03 23:04:57 -0500

    Remove link to Twitter on Post Author component

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -10,15 +10,8 @@ const Author = () => {
   return (
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
+        <strong>{author.name}</strong><br/>
         {author.bio}
-        <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong> on Twitter
-        </a>
       </p>
     </div>
   );
