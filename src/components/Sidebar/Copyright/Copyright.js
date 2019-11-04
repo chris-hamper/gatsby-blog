92ae1fc6b6b02e3c3dd4f4f20b3b1af464d47e31
Chris Hamper <chris.hamper@acquia.com>
2019-11-03 23:05:28 -0500

    Tweak license

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -8,10 +8,10 @@ type Props = {
 
 const Copyright = ({ copyright }) => (
   <div className={styles['copyright']}>
-    {copyright}<br/>
-    CC BY-SA 4.0 License
+    {copyright} {new Date().getFullYear()}<br/>
+    Licensed under CC BY-SA 4.0
     <a href="https://creativecommons.org/licenses/by-sa/4.0/">
-      <img src="https://licensebuttons.net/l/by-sa/4.0/88x31.png" alt="CC BY-SA 4.0 License"/>
+      <img src="https://licensebuttons.net/l/by-sa/4.0/88x31.png" alt="Licensed under CC BY-SA 4.0"/>
     </a>
   </div>
 );
